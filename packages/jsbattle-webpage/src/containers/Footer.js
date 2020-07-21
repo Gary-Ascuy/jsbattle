9187f2e893987156f97945fdc63134665aa50a85
Gary Ascuy <gary.ascuy@gmail.com>
2020-07-20 22:33:44 -0500

    Updaing branding

--- packages/jsbattle-webpage/src/containers/Footer.js
+++ packages/jsbattle-webpage/src/containers/Footer.js
@@ -8,7 +8,7 @@ export class Footer extends React.Component {
   render() {
     return <FullRow>
             <small style={{color: '#999', textAlign: 'center', borderTop: '1px solid #999', width: "100%", display: 'inline-block', padding: '5px'}}>
-              Hosted on <a href="https://github.com/jamro/jsbattle" target="_blank" rel="noopener noreferrer"><i className="fab fa-github" aria-hidden="true"></i> GitHub</a>.
+              Update Branding - Gary Ascuy, Hosted on <a href="https://github.com/jamro/jsbattle" target="_blank" rel="noopener noreferrer"><i className="fab fa-github" aria-hidden="true"></i> GitHub</a>.
               This project is licensed under the terms of the <a href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer">MIT license</a>.
               Version: {VERSION}
             </small>
